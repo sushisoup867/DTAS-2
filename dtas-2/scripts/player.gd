592ee135d5513fab203aadde313aa3df600f1562
XBotDoesStuff <Xanderchuck@gmail.com>
2025-03-28 15:22:27 -0600

    Started work on dashing mechanic
    As of right now it only works depending on camera direction rather than last moved direction

--- dtas-2/scripts/player.gd
+++ dtas-2/scripts/player.gd
@@ -2,12 +2,16 @@ extends CharacterBody3D
 
 # Import other components
 @onready var head: Node3D = $Head
+@onready var camera_3d: Camera3D = $Head/Camera3D
 
 # Movement variables
 var current_speed = 20.0
 @export var dashing_speed = 50.0
+@export var dash_duration = 0.1
 @export var running_speed = 20.0
 var jump_velocity = 4.5
+var dash_ready: bool = true
+var extra_vel = Vector3.ZERO
 
 var mouse_sensitivity = 0.25
 
@@ -34,16 +38,29 @@ func _unhandled_input(_event: InputEvent) -> void:
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		capture_mouse()
 
+func dash() -> void:
+	$DashTimer.wait_time = dash_duration
+	$DashTimer.start()
+	dash_ready = false
+
+func _on_dash_timer_timeout() -> void:
+	dash_ready = true
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
-
-	# Get the input direction and handle the movement/deceleration.
+	# Handle dashing.
+	if Input.is_action_just_pressed("dash") and dash_ready:
+		dash()
+	if $DashTimer.time_left > 0:
+		velocity += -transform.basis.z
+	
+	# Get the input direction and handle the movement/deceleration.~
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("strafe_left", "strafe_right", "move_forward", "move_backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
@@ -53,5 +70,5 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, current_speed)
 		velocity.z = move_toward(velocity.z, 0, current_speed)
-
+	
 	move_and_slide()

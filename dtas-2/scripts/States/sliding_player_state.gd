6f56fd305adddd4c714d9c1eb68c962edfb943f5
xbot-school-account <26siggaxanc@washk12.org>
2025-04-28 14:10:43 -0600

    added sliding animation
    E12 5:31

--- dtas-2/scripts/States/sliding_player_state.gd
+++ dtas-2/scripts/States/sliding_player_state.gd
@@ -10,9 +10,22 @@ extends PlayerMovementState
 
 func enter(previous_state) -> void:
 	set_tilt(player._current_rotation)
+	animation.get_animation('Sliding').track_set_key_value(4, 0, player.velocity.length())
+	animation.speed_scale = 1.0
+	animation.play('Sliding', -1.0, slide_anim_speed)
+
+func update(delta):
+	player.update_gravity(delta)
+	player.update_input(speed)
+	player.update_velocity()
 
 func set_tilt(player_rotation) -> void:
 	var tilt = Vector3.ZERO
 	tilt.z = clamp(tilt_amount * player_rotation, -0.1, 0.1)
 	if tilt.z == 0.0:
 		tilt.z == 0.05
+	animation.get_animation('Sliding').track_set_key_value(8, 1, tilt)
+	animation.get_animation('Sliding').track_set_key_value(8, 2, tilt)
+
+func finish():
+	transition.emit("CrouchingPlayerState")
